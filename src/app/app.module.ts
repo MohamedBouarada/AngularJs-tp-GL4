6c08892d5bc419af06aa942ae31f9d671095fe94
MohamedBouarada <bouarada7.mb@gmail.com>
2022-10-22 00:32:04 +0100

    ex2 done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FirstComponentComponent } from './first-component/first-component.component';
+import { CarteVisiteComponent } from './carte-visite/carte-visite.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @NgModule({
   declarations: [
     AppComponent,
-    FirstComponentComponent
+    FirstComponentComponent,
+    CarteVisiteComponent
   ],
   imports: [
+    CommonModule,
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/carte-visite/carte-visite.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-carte-visite',
+  templateUrl: './carte-visite.component.html',
+  styleUrls: ['./carte-visite.component.scss']
+})
+export class CarteVisiteComponent implements OnInit {
+  firstName:string = "Mohamed"
+  lastName:string = "Bouarada"
+  image:string = "mohamed.jpg"
+  job:string = "Software engineer"
+  quote:string = `"Lamborghini Mercy Your chick she so thirsty I'm in that two seat Lambo"`
+  jobDescription:string="etudiant en 2ème année cycle d'ingénieur en génie logiciel à l insat"
+  rotate:boolean=false
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+  setRotate(){
+    this.rotate=!this.rotate;
+  }
+
+}
